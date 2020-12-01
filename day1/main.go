73de715cffe17ec40bb96c2940e357ff29bc57a1
Gabor Javorszky <gabor@javorszky.co.uk>
2020-12-01 23:44:44 +0000

    day 1 task 2

--- day1/main.go
+++ day1/main.go
@@ -13,14 +13,35 @@ const target = 2020
 
 func main() {
 	m := getInputs()
-	for i := range m {
-		j := target - i
-		if _, ok := m[j]; ok == true {
-			fmt.Printf("found the pair: %d and %d. Their sum is %d and product is %d\n", i, j, i+j, i*j)
+	f := func(t int, arr map[int]struct{}) (int, int, error) {
+		for i := range arr {
+			j := t - i
+			if _, ok := arr[j]; ok == true {
+				return i, j, nil
+			}
+		}
+		return 0, 0, fmt.Errorf("no pair found for %d", t)
+	}
+
+	i1, i2, err := f(2020, m)
+	if err != nil {
+		fmt.Println("no pair for 2020")
+		os.Exit(1)
+	}
+
+	fmt.Printf("found the pair for %d: %d and %d. Their sum is %d and product is %d\n", target, i1, i2, i1+i2, i1*i2)
+
+	for k := range m {
+		l := target - k
+		o1, o2, err := f(l, m)
+		if err == nil {
+			fmt.Printf("found the triplet: %d, %d, and %d. Their sum is %d, and their product is %d\n", k, o1, o2, k+o1+o2, k*o1*o2)
 			os.Exit(0)
 		}
+
 	}
-	fmt.Println("found no pairs :(")
+	fmt.Println("found no triplets :(")
+	os.Exit(1)
 }
 
 func getInputs() map[int]struct{} {

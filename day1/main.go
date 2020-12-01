078c6e1ff337f06d11f02be7d3d5d87cf390e941
Gabor Javorszky <gabor@javorszky.co.uk>
2020-12-01 23:50:38 +0000

    add comments for day 1

--- day1/main.go
+++ day1/main.go
@@ -13,6 +13,8 @@ const target = 2020
 
 func main() {
 	m := getInputs()
+
+	// func to find 2 numbers is arr that sum to t.
 	f := func(t int, arr map[int]struct{}) (int, int, error) {
 		for i := range arr {
 			j := t - i
@@ -23,6 +25,7 @@ func main() {
 		return 0, 0, fmt.Errorf("no pair found for %d", t)
 	}
 
+	// task 1: find 2 numbers that sum to 2020.
 	i1, i2, err := f(2020, m)
 	if err != nil {
 		fmt.Println("no pair for 2020")
@@ -31,6 +34,8 @@ func main() {
 
 	fmt.Printf("found the pair for %d: %d and %d. Their sum is %d and product is %d\n", target, i1, i2, i1+i2, i1*i2)
 
+	// task 2: find 3 numbers that sum to 2020. For that take 1 number, subtract from 2020, then find 2 other numbers
+	// that sum to the remainder.
 	for k := range m {
 		l := target - k
 		o1, o2, err := f(l, m)
@@ -44,6 +49,7 @@ func main() {
 	os.Exit(1)
 }
 
+// getInputs reads the input.txt file, and arranges the contents into a map of unique numbers.
 func getInputs() map[int]struct{} {
 	data, err := ioutil.ReadFile(filename)
 	if err != nil {

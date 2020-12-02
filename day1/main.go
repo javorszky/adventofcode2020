92d6a27e45b0185ecb6c35aa4e1079a3869b373a
Gabor Javorszky <gabor@javorszky.co.uk>
2020-12-02 08:55:13 +0000

    make it more readable

--- day1/main.go
+++ day1/main.go
@@ -14,39 +14,20 @@ const target = 2020
 func main() {
 	m := getInputs()
 
-	// func to find 2 numbers is arr that sum to t.
-	f := func(t int, arr map[int]struct{}) (int, int, error) {
-		for i := range arr {
-			j := t - i
-			if _, ok := arr[j]; ok == true {
-				return i, j, nil
-			}
-		}
-		return 0, 0, fmt.Errorf("no pair found for %d", t)
-	}
-
-	// task 1: find 2 numbers that sum to 2020.
-	i1, i2, err := f(2020, m)
+	p1, p2, err := findPair(m)
 	if err != nil {
-		fmt.Println("no pair for 2020")
+		fmt.Printf("could not find pair... %s", err)
 		os.Exit(1)
 	}
+	fmt.Printf("found the pair for %d: %d and %d. Their sum is %d and product is %d\n", target, p1, p2, p1+p2, p1*p2)
 
-	fmt.Printf("found the pair for %d: %d and %d. Their sum is %d and product is %d\n", target, i1, i2, i1+i2, i1*i2)
-
-	// task 2: find 3 numbers that sum to 2020. For that take 1 number, subtract from 2020, then find 2 other numbers
-	// that sum to the remainder.
-	for k := range m {
-		l := target - k
-		o1, o2, err := f(l, m)
-		if err == nil {
-			fmt.Printf("found the triplet: %d, %d, and %d. Their sum is %d, and their product is %d\n", k, o1, o2, k+o1+o2, k*o1*o2)
-			os.Exit(0)
-		}
-
+	t1, t2, t3, err := findTriplet(m)
+	if err != nil {
+		fmt.Printf("could not find triplet: %s", err)
+		os.Exit(1)
 	}
-	fmt.Println("found no triplets :(")
-	os.Exit(1)
+	fmt.Printf("found the triplet: %d, %d, and %d. Their sum is %d, and their product is %d\n", t1, t2, t3, t1+t2+t3, t1*t2*t3)
+	os.Exit(0)
 }
 
 // getInputs reads the input.txt file, and arranges the contents into a map of unique numbers.
@@ -71,4 +52,32 @@ func getInputs() map[int]struct{} {
 	}
 
 	return intMap
+}
+
+// findPair will return the pair of numbers that sum to 2020 from the input map.
+func findPair(m map[int]struct{}) (int, int, error) {
+	return findPairFor(target, m)
+}
+
+// findTriple will return three numbers that sum to 2020 in input map.
+func findTriplet(m map[int]struct{}) (int, int, int, error) {
+	for k := range m {
+		l := target - k
+		o1, o2, err := findPairFor(l, m)
+		if err == nil {
+			return k, o1, o2, nil
+		}
+	}
+	return 0, 0, 0, fmt.Errorf("no triplet found for %d", target)
+}
+
+// findPairFor will return two integers that sum to t.
+func findPairFor(t int, arr map[int]struct{}) (int, int, error) {
+	for i := range arr {
+		j := t - i
+		if _, ok := arr[j]; ok == true {
+			return i, j, nil
+		}
+	}
+	return 0, 0, fmt.Errorf("no pair found for %d", t)
 }

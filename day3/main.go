c62d09ed8de959ba05a220beeafd812807f0e8d6
Gabor Javorszky <gabor@javorszky.co.uk>
2020-12-03 12:17:23 +0000

    added tests for istree and step, implement them

--- day3/main.go
+++ day3/main.go
@@ -6,6 +6,10 @@ import (
 )
 
 const filename = "input.txt"
+const startRow = 0
+const stepRow = 1
+const startCol = 0
+const stepCol = 3
 
 func main() {
 
@@ -21,8 +25,19 @@ func getInputs() []string {
 	return strings.Split(string(data), "\n")
 }
 
+func step(n int) (int, int) {
+	return startRow + n*stepRow, startCol + n*stepCol
+}
+
 // isTree returns whether the ground at the given coordinates is a tree or not. Column is arbitrary number, is not
 // constrained by width of the map.
 func isTree(row, column int, slope []string) bool {
-	return false
+	if len(slope) < row {
+		return false
+	}
+	trueColumn := column % len(slope[0])
+
+	r := slope[row]
+
+	return "#" == string(r[trueColumn])
 }
--- day3/main_test.go
+++ day3/main_test.go
@@ -51,14 +51,17 @@ func Test_isTree(t *testing.T) {
 				column: 11,
 				slope:  []string{"#......##."},
 			},
-			want: true,
+			want: false,
 		},
 		{
 			name: "correctly returns tree in row 1 not overflown",
 			args: args{
 				row:    1,
 				column: 4,
-				slope:  []string{"#......##.", "....#..##."},
+				slope: []string{
+					"#......##.",
+					"....#..##.",
+				},
 			},
 			want: true,
 		},
@@ -67,15 +70,54 @@ func Test_isTree(t *testing.T) {
 			args: args{
 				row:    1,
 				column: 3,
-				slope:  []string{"#......##.", "....#..##."},
+				slope: []string{
+					"#......##.",
+					"....#..##.",
+				},
 			},
-			want: true,
+			want: false,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			assert.Equal(t, tt.want, isTree(tt.args.row, tt.args.column, tt.args.slope))
+		})
+	}
+}
+
+func Test_step(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  int
+		want1 int
+	}{
+		{
+			name: "correctly gets result for step 1",
+			args: args{
+				n: 1,
+			},
+			want:  1,
+			want1: 3,
+		},
+		{
+			name: "correctly gets result for step 6",
+			args: args{
+				n: 6,
+			},
+			want:  6,
+			want1: 18,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := step(tt.args.n)
+			assert.Equal(t, tt.want, got)
+			assert.Equal(t, tt.want1, got1)
 		})
 	}
 }

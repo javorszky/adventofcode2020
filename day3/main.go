d5f167aba8bc78e6d10a81e5489526baa67c3158
Gabor Javorszky <gabor@javorszky.co.uk>
2020-12-03 12:52:17 +0000

    day3 task 2 done

--- day3/main.go
+++ day3/main.go
@@ -14,6 +14,12 @@ const stepCol = 3
 
 func main() {
 	slope := getInputs()
+	task1(slope)
+
+	task2(slope)
+}
+
+func task1(slope []string) {
 	trees := 0
 	for i := 0; i < len(slope); i++ {
 		r, c := step(i)
@@ -21,7 +27,32 @@ func main() {
 			trees++
 		}
 	}
-	fmt.Printf("We encountered %d trees.", trees)
+	fmt.Printf("We encountered %d trees.\n", trees)
+}
+
+func task2(slope []string) {
+
+	steps := [][]int{
+		{1, 1},
+		{1, 3},
+		{1, 5},
+		{1, 7},
+		{2, 1},
+	}
+	pTrees := 1
+	for _, step := range steps {
+		localTree := 0
+		for i := 0; i < len(slope); i++ {
+			r, c := varStep(i, step[0], step[1])
+			if isTree(r, c, slope) {
+				localTree++
+			}
+		}
+
+		pTrees = pTrees * localTree
+	}
+
+	fmt.Printf("after having checked all the slopes, the product is %d\n", pTrees)
 }
 
 // getInputs reads the input.txt file, and arranges the contents into a map of unique numbers.
@@ -45,7 +76,7 @@ func varStep(n, varStepRow, varStepCol int) (int, int) {
 // isTree returns whether the ground at the given coordinates is a tree or not. Column is arbitrary number, is not
 // constrained by width of the map.
 func isTree(row, column int, slope []string) bool {
-	if len(slope) < row {
+	if len(slope) <= row {
 		return false
 	}
 	trueColumn := column % len(slope[0])

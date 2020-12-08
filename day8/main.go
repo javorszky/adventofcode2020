7c8d22389d193905001da7568dd2f0000287c93e
Gabor Javorszky <gabor@javorszky.co.uk>
2020-12-08 14:31:37 +0000

    day 8 task 2 wip

--- day8/main.go
+++ day8/main.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
-	"os"
 	"strconv"
 	"strings"
 )
@@ -25,7 +24,15 @@ func (i infiniteLoop) Error() string {
 	return fmt.Sprintf("infinite loop error: %s", i.Message)
 }
 
-func task1() {
+type terminate struct {
+	Message string
+}
+
+func (t terminate) Error() string {
+	return fmt.Sprintf("next instruction out of bounds: %s", t.Message)
+}
+
+func task1() int {
 	// instructions line by line as a slice of strings.
 	instructions := getInputs()
 
@@ -42,16 +49,15 @@ func task1() {
 	var err error
 
 	// create reusable function to execute the code. Accumulator and instruction set are outside of this func.
-
 	for {
 		acc, l, jumps, err = run(acc, l, jumps, instructions)
 		if errors.As(err, &infiniErr) {
 			fmt.Printf("day 8 task 1: infinite code current state of accumulator is %d", acc)
-			os.Exit(0)
+			return 0
 		}
 		if err != nil {
 			fmt.Printf("big oof, bad code: %s", err)
-			os.Exit(1)
+			return 1
 		}
 	}
 }
@@ -67,12 +73,56 @@ func t1Number(s string) (int, error) {
 	return n, nil
 }
 
-func task2() {
+func task2() int {
+	// instructions line by line as a slice of strings.
+	instructions := getInputs()
+	replacer := strings.NewReplacer("jmp", "nop", "nop", "jmp")
+	// let's start brute forcing the program changes
+	for i, currentLine := range instructions {
+		// we don't want to deal with acc lines.
+		if strings.HasPrefix(currentLine, "acc") {
+			continue
+		}
 
+		// copy the program so we can swap one line without affecting the source code.
+		changedInstructions := make([]string, len(instructions))
+		copy(changedInstructions, instructions)
+
+		// replace that one line.
+		changedInstructions[i] = replacer.Replace(changedInstructions[i])
+
+		// accumulator starts at 0.
+		acc := 0
+
+		// keep track of previous code executions so we know when we're about to hit an infinite loop.
+		jumps := make(map[int]struct{}, 0)
+
+		// kick off running the program on line 0.
+		l := 0
+
+		var term terminate
+		var err error
+
+		// create reusable function to execute the code. Accumulator and instruction set are outside of this func.
+		for {
+			acc, l, jumps, err = run(acc, l, jumps, changedInstructions)
+			if errors.As(err, &term) {
+				fmt.Printf("day 8 task 2: program terminated.\nHad to change line %d from\n%s to\n%s\nAccumulator is %d", i, instructions[i], changedInstructions[i], acc)
+				return 0
+			}
+			break
+		}
+	}
+	fmt.Printf("um, couldn't find the thing... something's wrong")
+	return 1
 }
 
 func run(acc, line int, jumps map[int]struct{}, program []string) (int, int, map[int]struct{}, error) {
 	var next int
+
+	if line == len(program)-1 {
+		return acc, next, jumps, terminate{Message: fmt.Sprintf("program is terminating. Acc is %d", acc)}
+	}
 
 	// have we run this previously?
 	if _, ok := jumps[line]; ok == true {

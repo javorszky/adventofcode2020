c941bab77a5cb87c40ceaba1bfea941717021481
Gabor Javorszky <gabor@javorszky.co.uk>
2020-12-11 11:28:55 +0000

    day 11 task 1 done

--- day11/main.go
+++ day11/main.go
@@ -32,117 +32,19 @@ func Tasks() {
 }
 
 func task1() {
-	_ = getInputs()
-}
+	lineLength, input := t1normalize(getInputs())
+	previous := input
+	newState := ""
+	for {
+		newState = t1flip(lineLength, previous)
 
-// t1normalize returns one string and a line length to be used for all the other bits. This assumes that the input slice
-// is uniform in its line length.
-func t1normalize(input []string) (int, string) {
-	lineLength := len(input[0])
-	var sb strings.Builder
-	for _, line := range input {
-		sb.WriteString(line)
-	}
-	return lineLength, sb.String()
-}
-
-func topLeft(c, lineLength int, input string) string {
-	tl := c - lineLength - 1
-	// if we're out of bounds or in the left column.
-	if 0 > tl || len(input)-1 < tl || c%lineLength == 0 {
-		return ""
+		if previous == newState {
+			break
+		}
+		previous = newState
 	}
 
-	return string(input[tl])
-}
-
-func top(c, lineLength int, input string) string {
-	tl := c - lineLength
-	// if we're out of bounds or in the left column.
-	if 0 > tl || len(input)-1 < tl {
-		return ""
-	}
-
-	return string(input[tl])
-}
-
-func topRight(c, lineLength int, input string) string {
-	tl := c - lineLength + 1
-	// if we're out of bounds or in the left column.
-	if 0 > tl || len(input)-1 < tl || c%lineLength == lineLength-1 {
-		return ""
-	}
-
-	return string(input[tl])
-}
-
-func right(c, lineLength int, input string) string {
-	tl := c + 1
-	// if we're out of bounds or in the left column.
-	if 0 > tl || len(input)-1 < tl || c%lineLength == lineLength-1 {
-		return ""
-	}
-
-	return string(input[tl])
-}
-
-func bottomRight(c, lineLength int, input string) string {
-	tl := c + lineLength + 1
-	// if we're out of bounds or in the left column.
-	if 0 > tl || len(input)-1 < tl || c%lineLength == lineLength-1 {
-		return ""
-	}
-
-	return string(input[tl])
-}
-
-func bottom(c, lineLength int, input string) string {
-	tl := c + lineLength
-	// if we're out of bounds or in the left column.
-	if 0 > tl || len(input)-1 < tl {
-		return ""
-	}
-
-	return string(input[tl])
-}
-
-func bottomLeft(c, lineLength int, input string) string {
-	tl := c + lineLength - 1
-	// if we're out of bounds or in the left column.
-	if 0 > tl || len(input)-1 < tl || c%lineLength == 0 {
-		return ""
-	}
-
-	return string(input[tl])
-}
-
-func left(c, lineLength int, input string) string {
-	tl := c - 1
-	// if we're out of bounds or in the left column.
-	if 0 > tl || len(input)-1 < tl || c%lineLength == 0 {
-		return ""
-	}
-
-	return string(input[tl])
-}
-
-// t1adjacent returns the number of occupied seats adjacent to the one specified by the coordinates. The coordinates h,v
-// stand for horizontal and vertical. Horizontal is a character inside of a line of string, vertical is an entire line
-// of string inside the slice of strings.
-//
-// Returns error if the coordinate is a floor, or if the coordinate is out of bounds.
-func t1adjacent(h, v int, state []string) (int, error) {
-	if v < 0 || v > len(state)-1 {
-		return 0, outOfBounds{Message: fmt.Sprintf("vertical is out of bounds: %d", v)}
-	}
-	if h < 0 || h > len(state[0])-1 {
-		return 0, outOfBounds{Message: fmt.Sprintf("horizontal is out of bounds: %d", h)}
-	}
-
-	if string(state[v][h]) == "." {
-		return 0, floorIsLave{Message: fmt.Sprintf("spot at coordinate h: %d, v: %d is a floor", h, v)}
-	}
-	return 0, nil
+	fmt.Printf("\nDay 11 task 1: once stabilized, there are %d occupied seats\n", strings.Count(newState, "#"))
 }
 
 func task2() {
---
+++ day11/traverse.go
@@ -0,0 +1,139 @@
+package day11
+
+import "strings"
+
+// t1normalize returns one string and a line length to be used for all the other bits. This assumes that the input slice
+// is uniform in its line length.
+func t1normalize(input []string) (int, string) {
+	lineLength := len(input[0])
+	var sb strings.Builder
+	for _, line := range input {
+		sb.WriteString(line)
+	}
+	return lineLength, sb.String()
+}
+
+func topLeft(c, lineLength int, input string) string {
+	tl := c - lineLength - 1
+	// if we're out of bounds or in the left column.
+	if 0 > tl || len(input)-1 < tl || c%lineLength == 0 {
+		return ""
+	}
+
+	return string(input[tl])
+}
+
+func top(c, lineLength int, input string) string {
+	tl := c - lineLength
+	// if we're out of bounds or in the left column.
+	if 0 > tl || len(input)-1 < tl {
+		return ""
+	}
+
+	return string(input[tl])
+}
+
+func topRight(c, lineLength int, input string) string {
+	tl := c - lineLength + 1
+	// if we're out of bounds or in the left column.
+	if 0 > tl || len(input)-1 < tl || c%lineLength == lineLength-1 {
+		return ""
+	}
+
+	return string(input[tl])
+}
+
+func right(c, lineLength int, input string) string {
+	tl := c + 1
+	// if we're out of bounds or in the left column.
+	if 0 > tl || len(input)-1 < tl || c%lineLength == lineLength-1 {
+		return ""
+	}
+
+	return string(input[tl])
+}
+
+func bottomRight(c, lineLength int, input string) string {
+	tl := c + lineLength + 1
+	// if we're out of bounds or in the left column.
+	if 0 > tl || len(input)-1 < tl || c%lineLength == lineLength-1 {
+		return ""
+	}
+
+	return string(input[tl])
+}
+
+func bottom(c, lineLength int, input string) string {
+	tl := c + lineLength
+	// if we're out of bounds or in the left column.
+	if 0 > tl || len(input)-1 < tl {
+		return ""
+	}
+
+	return string(input[tl])
+}
+
+func bottomLeft(c, lineLength int, input string) string {
+	tl := c + lineLength - 1
+	// if we're out of bounds or in the left column.
+	if 0 > tl || len(input)-1 < tl || c%lineLength == 0 {
+		return ""
+	}
+
+	return string(input[tl])
+}
+
+func left(c, lineLength int, input string) string {
+	tl := c - 1
+	// if we're out of bounds or in the left column.
+	if 0 > tl || len(input)-1 < tl || c%lineLength == 0 {
+		return ""
+	}
+
+	return string(input[tl])
+}
+
+// t1adjacent returns the number of occupied seats adjacent to the one specified by the coordinates. The coordinates h,v
+// stand for horizontal and vertical. Horizontal is a character inside of a line of string, vertical is an entire line
+// of string inside the slice of strings.
+//
+// Returns error if the coordinate is a floor, or if the coordinate is out of bounds.
+func t1adjacentOccupied(c, lineLength int, input string) int {
+	var sb strings.Builder
+	sb.WriteString(top(c, lineLength, input))
+	sb.WriteString(topRight(c, lineLength, input))
+	sb.WriteString(right(c, lineLength, input))
+	sb.WriteString(bottomRight(c, lineLength, input))
+	sb.WriteString(bottom(c, lineLength, input))
+	sb.WriteString(bottomLeft(c, lineLength, input))
+	sb.WriteString(left(c, lineLength, input))
+	sb.WriteString(topLeft(c, lineLength, input))
+	return strings.Count(sb.String(), "#")
+}
+
+func t1flip(lineLength int, input string) string {
+	var sb strings.Builder
+
+	for idx, c := range input {
+		// floors never change
+		switch string(c) {
+		case ".":
+			sb.WriteString(".")
+		case "L":
+			// field is empty, can we make it occupied?
+			if t1adjacentOccupied(idx, lineLength, input) == 0 {
+				sb.WriteString("#")
+			} else {
+				sb.WriteString("L")
+			}
+		case "#":
+			// field is occupied, is it going to be empty?
+			if t1adjacentOccupied(idx, lineLength, input) >= 4 {
+				sb.WriteString("L")
+			} else {
+				sb.WriteString("#")
+			}
+		}
+	}
+	return sb.String()
+}

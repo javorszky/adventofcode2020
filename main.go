527f5ff913fe860ef0db819d3e586d00651b1767
Gabor Javorszky <gabor@javorszky.co.uk>
2020-12-12 15:58:54 +0000

    uncomment calls to previous days

--- main.go
+++ main.go
@@ -1,31 +1,42 @@
 package main
 
 import (
+	"github.com/javorszky/adventofcode2020/day1"
+	"github.com/javorszky/adventofcode2020/day10"
+	"github.com/javorszky/adventofcode2020/day11"
 	"github.com/javorszky/adventofcode2020/day12"
+	"github.com/javorszky/adventofcode2020/day2"
+	"github.com/javorszky/adventofcode2020/day3"
+	"github.com/javorszky/adventofcode2020/day4"
+	"github.com/javorszky/adventofcode2020/day5"
+	"github.com/javorszky/adventofcode2020/day6"
+	"github.com/javorszky/adventofcode2020/day7"
+	"github.com/javorszky/adventofcode2020/day8"
+	"github.com/javorszky/adventofcode2020/day9"
 )
 
 func main() {
-	//day1.Tasks()
-	//
-	//day2.Tasks()
-	//
-	//day3.Tasks()
-	//
-	//day4.Tasks()
-	//
-	//day5.Tasks()
-	//
-	//day6.Tasks()
-	//
-	//day7.Tasks()
-	//
-	//day8.Tasks()
-	//
-	//day9.Tasks()
-	//
-	//day10.Tasks()
+	day1.Tasks()
 
-	//day11.Tasks()
+	day2.Tasks()
+
+	day3.Tasks()
+
+	day4.Tasks()
+
+	day5.Tasks()
+
+	day6.Tasks()
+
+	day7.Tasks()
+
+	day8.Tasks()
+
+	day9.Tasks()
+
+	day10.Tasks()
+
+	day11.Tasks()
 
 	day12.Tasks()
 }

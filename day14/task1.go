d9b29764bd54f144d75118067fefb08295c4bf97
Gabor Javorszky <gabor@javorszky.co.uk>
2020-12-14 17:34:02 +0000

    add day 14 task 1 tests and functions

--- day14/task1.go
+++ day14/task1.go
@@ -1,6 +1,10 @@
 package day14
 
-import "strconv"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func task1() {
 	_ = getInputs()
@@ -10,4 +14,30 @@ func decimalToBinary(i int64) string {
 	b := strconv.FormatInt(i, 2)
 
 	return b[len(b)-36:]
+}
+
+func binaryToDecimal(s string) int64 {
+	d, err := strconv.ParseInt(s[len(s)-36:], 2, 64)
+	if err != nil {
+		panic(fmt.Sprintf("converting %s to binary failed: %s", s, err))
+	}
+	return d
+}
+
+func applyMask(mask, binary string) string {
+	if len(mask) != len(binary) {
+		panic(fmt.Sprintf("mask length (%d) and binary lengths (%d) differ in applying mask", len(mask), len(binary)))
+	}
+	var sb strings.Builder
+	for idx, char := range mask {
+		switch string(char) {
+		case "1":
+			sb.WriteString("1")
+		case "0":
+			sb.WriteString("0")
+		default:
+			sb.WriteString(string(binary[idx]))
+		}
+	}
+	return sb.String()
 }
--- day14/task1_test.go
+++ day14/task1_test.go
@@ -21,7 +21,7 @@ func Test_decimalToBinary(t *testing.T) {
 				// 2^37-1, should overflow
 				i: 137438953472 - 1,
 			},
-			want: "111111111111111111111111111111111111",
+			want: "11111111111111111111111111111111111",
 		},
 		{
 			name: "converts decimal to binary string",
@@ -29,12 +29,83 @@ func Test_decimalToBinary(t *testing.T) {
 				// 2^41, should overflow
 				i: 2199023255552,
 			},
-			want: "000000000000000000000000000000000000",
+			want: "00000000000000000000000000000000000",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			assert.Equal(t, tt.want, decimalToBinary(tt.args.i))
+		})
+	}
+}
+
+func Test_binaryToDecimal(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		{
+			name: "converts all zero binary string to decimal",
+			args: args{
+				s: "000000000000000000000000000000000000",
+			},
+			want: 0,
+		},
+		{
+			name: "converts all 1 binary string to decimal",
+			args: args{
+				s: "111111111111111111111111111111111111",
+			},
+			want: 68719476735,
+		},
+		{
+			name: "converts too long all 1 binary string to decimal",
+			args: args{
+				s: "1111111111111111111111111111111111111111111111111",
+			},
+			want: 68719476735,
+		},
+		{
+			name: "converts too long all 1 binary string to decimal cutting off correct end",
+			args: args{
+				s: "1100000000000000000000000000000000000000000000011",
+			},
+			want: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, binaryToDecimal(tt.args.s))
+		})
+	}
+}
+
+func Test_applyMask(t *testing.T) {
+	type args struct {
+		mask   string
+		binary string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "applies mask correctly",
+			args: args{
+				mask:   "10X10X",
+				binary: "111000",
+			},
+			want: "101100",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, applyMask(tt.args.mask, tt.args.binary))
 		})
 	}
 }

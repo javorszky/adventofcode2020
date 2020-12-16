9cb8b8141f0254bfe908bd16a2648d0468a18666
Gabor Javorszky <gabor@javorszky.co.uk>
2020-12-16 11:33:34 +0000

    day 16 task 1 done

--- day16/task1.go
+++ day16/task1.go
@@ -3,19 +3,23 @@ package day16
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 func task1() {
 	values := make(map[int]struct{}, 0)
 	//rules, myTicket, nearbyTickets := getInputs()
-	rules, _, _ := getInputs()
+	rules, _, nearbyTickets := getInputs()
 	for _, rule := range rules {
 		matches := reRule.FindStringSubmatch(rule)
-		fmt.Printf("%#v\n\n", matches)
-
-		values = mergeRanges(values, matches[0], matches[1])
 		values = mergeRanges(values, matches[2], matches[3])
+		values = mergeRanges(values, matches[4], matches[5])
 	}
+
+	serint, sermap := checkNearbyTickets(values, nearbyTickets)
+	fmt.Printf("\nDay 16 task 1\n"+
+		"scanning error rate when duplicate values are counted each time: %d (this was the correct one)\n"+
+		"scanning error rate when duplicate values are counted only once: %d\n", serint, sermap)
 }
 
 func mergeRanges(values map[int]struct{}, min, max string) map[int]struct{} {
@@ -40,4 +44,29 @@ func mergeRanges(values map[int]struct{}, min, max string) map[int]struct{} {
 		values[i] = struct{}{}
 	}
 	return values
+}
+
+func checkNearbyTickets(values map[int]struct{}, nearbyTickets []string) (int, int) {
+	sumMap := make(map[int]struct{}, 0)
+	sum := 0
+	for _, ticket := range nearbyTickets {
+		tValues := strings.Split(ticket, ",")
+		for _, tValue := range tValues {
+			tv, err := strconv.Atoi(tValue)
+			if err != nil {
+				panic(fmt.Sprintf("checknearbytickets: %s, err: %s", tValue, err))
+			}
+			if _, ok := values[tv]; !ok {
+				sum = sum + tv
+				sumMap[tv] = struct{}{}
+			}
+		}
+	}
+
+	summ := 0
+	for idx := range sumMap {
+		summ = summ + idx
+	}
+
+	return sum, summ
 }

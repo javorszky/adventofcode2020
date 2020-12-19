c450b101a31d88d58454c2b86de664064de87590
Gabor Javorszky <gabor@javorszky.co.uk>
2020-12-19 20:16:40 +0000

    day 19 task 1 find rule func implemented and tested

--- day19/task1.go
+++ day19/task1.go
@@ -1,6 +1,14 @@
 package day19
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+var (
+	reParseRuleString = regexp.MustCompile(`^(\d+): (.*)$`)
+)
 
 // task1 will do the calculations necessary to compute day 19 task 1:
 //
@@ -8,4 +16,37 @@ import "fmt"
 func task1() {
 	rules, messages := getInputs()
 	fmt.Printf("rules:\n%#v\n\nmessages:\n%#v\n", rules, messages)
+}
+
+func parseRules(ruleStrings []string) map[string]string {
+	rules := make(map[string]string, 0)
+	for _, ruleString := range ruleStrings {
+		matches := reParseRuleString.FindStringSubmatch(ruleString)
+		rules[matches[1]] = matches[2]
+	}
+
+	return rules
+}
+
+func findRule(s string, rules map[string]string) string {
+	rule, ok := rules[s]
+	if !ok {
+		panic(fmt.Sprintf("could not find rule %s in rules map", s))
+	}
+	switch rule {
+	case "a", "b":
+		return strings.Trim(rule, `"`)
+	default:
+		ors := strings.Split(rule, " | ")
+		orsStrings := make([]string, 0)
+		for _, or := range ors {
+			numbers := strings.Split(or, " ")
+			numbersStrings := make([]string, 0)
+			for _, number := range numbers {
+				numbersStrings = append(numbersStrings, findRule(number, rules))
+			}
+			orsStrings = append(orsStrings, strings.Join(numbersStrings, " "))
+		}
+		return "( " + strings.Join(orsStrings, " | ") + " )"
+	}
 }
---
+++ day19/task1_test.go
@@ -0,0 +1,125 @@
+package day19
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_parseRules(t *testing.T) {
+	type args struct {
+		ruleStrings []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want map[string]string
+	}{
+		{
+			name: "parses rule string slice correctly",
+			args: args{
+				ruleStrings: []string{
+					"1: 1 2 | 3 4",
+					"2: \"a\"",
+				},
+			},
+			want: map[string]string{
+				"1": "1 2 | 3 4",
+				"2": "\"a\"",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, parseRules(tt.args.ruleStrings))
+		})
+	}
+}
+
+func Test_findRule(t *testing.T) {
+	type args struct {
+		s     string
+		rules map[string]string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "finds rule where rule is 'a'",
+			args: args{
+				s: "0",
+				rules: map[string]string{
+					"0": "a",
+				},
+			},
+			want: `a`,
+		},
+		{
+			name: "finds rule where rule is 'b'",
+			args: args{
+				s: "0",
+				rules: map[string]string{
+					"0": "b",
+				},
+			},
+			want: `b`,
+		},
+		{
+			name: "finds rule where rule is 'a' through substitution",
+			args: args{
+				s: "0",
+				rules: map[string]string{
+					"0": "1",
+					"1": "a",
+				},
+			},
+			want: `( a )`,
+		},
+		{
+			name: "finds rule where rule is 'a' and 'b' through substitutions with an or clause ",
+			args: args{
+				s: "0",
+				rules: map[string]string{
+					"0": "1 | 2",
+					"1": "a",
+					"2": "b",
+				},
+			},
+			want: `( a | b )`,
+		},
+		{
+			name: "finds rule where rule is 'a' and 'b' through substitutions with an or clause and repetitions",
+			args: args{
+				s: "0",
+				rules: map[string]string{
+					"0": "1 1 | 2 2",
+					"1": "a",
+					"2": "b",
+				},
+			},
+			want: `( a a | b b )`,
+		},
+		{
+			name: "finds rule where rule is 'a' and 'b' through substitutions with an or clause and repetitions in multiple levels",
+			args: args{
+				s: "0",
+				rules: map[string]string{
+					"0": "1 1 | 2 2",
+					"1": "3 4 | 4 3",
+					"2": "b",
+					"3": "2 5",
+					"4": "3 2 | 2 3",
+					"5": "a",
+				},
+			},
+			want: `( ( ( b a ) ( ( b a ) b | b ( b a ) ) | ( ( b a ) b | b ( b a ) ) ( b a ) ) ( ( b a ) ( ( b a ) b | b ( b a ) ) | ( ( b a ) b | b ( b a ) ) ( b a ) ) | b b )`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, findRule(tt.args.s, tt.args.rules))
+		})
+	}
+}

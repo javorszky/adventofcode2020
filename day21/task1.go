92ff8b76400f610c8d7151feeae7750d734821e4
Gabor Javorszky <gabor@javorszky.co.uk>
2020-12-22 00:38:55 +0000

    add tests for day 21 task 1

--- day21/task1.go
+++ day21/task1.go
@@ -19,3 +19,30 @@ func parseInputs(list []string) map[string][][]string {
 	}
 	return orderedList
 }
+
+// commonElements takes two string slices, and returns a third slice that has the common elements from both of them.
+func commonElements(a, b []string) []string {
+	n := make([]string, 0)
+	helper := make(map[string]struct{}, 0)
+	for _, v := range a {
+		helper[v] = struct{}{}
+	}
+
+	for _, v := range b {
+		if _, ok := helper[v]; ok {
+			n = append(n, v)
+		}
+	}
+
+	return n
+}
+
+// reduceElementsToCommon will take a slice of slices, and only return a slice of elements that are present in all of
+// the passed slices.
+func reduceElementsToCommon(slices ...[]string) []string {
+	toCheck := slices[0]
+	for _, sl := range slices {
+		toCheck = commonElements(toCheck, sl)
+	}
+	return toCheck
+}
--- day21/task1_test.go
+++ day21/task1_test.go
@@ -68,3 +68,105 @@ func Test_parseInputs(t *testing.T) {
 		})
 	}
 }
+
+func Test_commonElements(t *testing.T) {
+	type args struct {
+		a []string
+		b []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "returns common elements from both slices",
+			args: args{
+				a: []string{
+					"a", "b", "c", "d",
+				},
+				b: []string{
+					"a", "c", "e", "f", "q",
+				},
+			},
+			want: []string{
+				"a", "c",
+			},
+		},
+		{
+			name: "returns common elements from both slices if the slices are reversed",
+			args: args{
+				b: []string{
+					"a", "b", "c", "d",
+				},
+				a: []string{
+					"a", "c", "e", "f", "q",
+				},
+			},
+			want: []string{
+				"a", "c",
+			},
+		},
+		{
+			name: "returns common elements from both slices where there are no common elements",
+			args: args{
+				b: []string{
+					"a", "b", "c", "d",
+				},
+				a: []string{
+					"x", "y", "e", "f", "q",
+				},
+			},
+			want: []string{},
+		},
+		{
+			name: "returns common elements from both slices where one slice completely contains the other",
+			args: args{
+				b: []string{
+					"a", "b", "c", "d",
+				},
+				a: []string{
+					"a", "b", "c", "d", "x", "y", "e", "f", "q",
+				},
+			},
+			want: []string{
+				"a", "b", "c", "d",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, commonElements(tt.args.a, tt.args.b))
+		})
+	}
+}
+
+func Test_reduceElementsToCommon(t *testing.T) {
+	type args struct {
+		slices [][]string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "reduces slice of slices to their commone elements",
+			args: args{
+				slices: [][]string{
+					{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"},
+					{"a", "c", "e", "g", "i", "j"},
+					{"n", "m", "l", "k", "j", "i"},
+				},
+			},
+			want: []string{
+				"j", "i",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, reduceElementsToCommon(tt.args.slices...))
+		})
+	}
+}

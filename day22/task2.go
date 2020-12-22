0fd1f29113882614933a5440c970229bf73f1fd0
Gabor Javorszky <gabor@javorszky.co.uk>
2020-12-22 14:12:23 +0000

    day 22 t2 implement example mechanics

--- day22/task2.go
+++ day22/task2.go
@@ -1,13 +1,33 @@
 package day22
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 )
 
 func task2() {
-	//plays := make(map[string]struct{}, 0)
-	//p1, p2 := getInputs()
+	p1, p2 := getInputs()
+
+	winner, p1, p2 := game(p1, p2, 1)
+
+	message := ""
+	switch winner {
+	case 0:
+		if len(p2) == 0 {
+			message = fmt.Sprintf("player 1 won with a score of %d", calculateScore(p1))
+		} else {
+			message = fmt.Sprintf("player 2 won with a score of %d", calculateScore(p2))
+		}
+	case 1:
+		message = fmt.Sprintf("player 1 won with a score of %d", calculateScore(p1))
+	case 2:
+		message = fmt.Sprintf("player 2 won with a score of %d", calculateScore(p2))
+	default:
+		panic(fmt.Sprintf("unexpected winner value: %d", winner))
+	}
+
+	fmt.Printf("\nDay 22 task 2: %s\n", message)
 }
 
 func playString(p1, p2 []int) string {
@@ -25,4 +45,88 @@ func playString(p1, p2 []int) string {
 	sb.WriteString(strings.Join(stringHelper, ","))
 
 	return sb.String()
+}
+
+func game(p1Deck, p2Deck []int, level int) (int, []int, []int) {
+	plays := make(map[string]struct{}, 0)
+	p1Card, p2Card := 0, 0
+	var err error
+	round := 0
+	for {
+		round++
+		fmt.Printf("------- Round %3d of Game %2d -------\nbeginning a new iteration with decks:\np1: %v\np2: %v\n\n", round, level, p1Deck, p2Deck)
+		// if either of the decks is now empty, stop the game, we have won
+		if len(p1Deck) == 0 {
+			fmt.Printf("p1's deck is now empty, returning 2 as player 2 won, %v, %v\n", p1Deck, p2Deck)
+			return 2, p1Deck, p2Deck
+		}
+		if len(p2Deck) == 0 {
+			fmt.Printf("p2's deck is now empty, returning 1 as player 1 won, %v, %v\n", p1Deck, p2Deck)
+			return 1, p1Deck, p2Deck
+		}
+
+		// check if we've played this before
+		if _, ok := plays[playString(p1Deck, p2Deck)]; ok {
+			// player 1 won
+			fmt.Printf("we've played this before, returning 1, %v, %v\n", p1Deck, p2Deck)
+			return 1, p1Deck, p2Deck
+		}
+
+		// save the play as a unique play in the local map.
+		plays[playString(p1Deck, p2Deck)] = struct{}{}
+
+		// draw cards from both
+		p1Card, p1Deck, err = slicePop(p1Deck)
+		if err != nil {
+			panic(fmt.Sprintf("could not pop card from p1's deck %#v: %s", p1Deck, err))
+		}
+		p2Card, p2Deck, err = slicePop(p2Deck)
+		if err != nil {
+			panic(fmt.Sprintf("could not pop card from p2's deck %#v: %s", p2Deck, err))
+		}
+
+		// If both players have at least as many cards remaining in their deck as the value of the card they just drew,
+		// the winner of the round is determined by playing a new game of Recursive Combat (see below).
+		if p1Card <= len(p1Deck) && p2Card <= len(p2Deck) {
+			fmt.Printf("new recursive game!\np1card: %2d, len p1deck: %2d\np1deck: %v\n\np2card: %2d, len p2deck: %2d\np2deck: %v\n\n",
+				p1Card,
+				len(p1Deck),
+				p1Deck,
+				p2Card,
+				len(p2Deck),
+				p2Deck,
+			)
+			p1Copy := make([]int, p1Card)
+			p2Copy := make([]int, p2Card)
+			copy(p1Copy, p1Deck)
+			copy(p2Copy, p2Deck)
+
+			winner, _, _ := game(p1Copy, p2Copy, level+1)
+
+			fmt.Printf("returned from Game %d\np1deck: %v\np2deck: %v\np1card: %d\np2card: %d\nwinner: %d\n", level+1, p1Deck, p2Deck, p1Card, p2Card, winner)
+			switch winner {
+			case 1:
+				p1Deck = sliceAdd(p1Deck, p1Card, p2Card)
+			case 2:
+				p2Deck = sliceAdd(p2Deck, p2Card, p1Card)
+			default:
+				panic(fmt.Sprintf("unexpected winner value: %d", winner))
+			}
+			fmt.Printf("player %d won Round %d of game %d\np1: %v\np2: %v\n\n", winner, round, level, p1Deck, p2Deck)
+			continue
+		}
+
+		// no winner yet for other reasons, so let's compare the drawn cards
+		// if player 1 won, then put the two cards into player one's deck
+		if p1Card > p2Card {
+			fmt.Printf("no winner yet, p1 won with higher card\n")
+			p1Deck = sliceAdd(p1Deck, p1Card, p2Card)
+		} else {
+			fmt.Printf("no winner yet, p2 won with higher card\n")
+			p2Deck = sliceAdd(p2Deck, p2Card, p1Card)
+		}
+		fmt.Printf("cards:\np1: %2d\np2: %2d\n", p1Card, p2Card)
+
+		fmt.Printf("new decks:\np1: %v\np2: %v\n", p1Deck, p2Deck)
+	}
 }
--- day22/task2_test.go
+++ day22/task2_test.go
@@ -1,6 +1,7 @@
 package day22
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -28,6 +29,47 @@ func Test_playString(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			assert.Equal(t, tt.want, playString(tt.args.p1, tt.args.p2))
+		})
+	}
+}
+
+func Test_game(t *testing.T) {
+	type args struct {
+		p1Deck []int
+		p2Deck []int
+		level  int
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  int
+		want1 []int
+		want2 []int
+	}{
+		{
+			name: "plays recursive combat per aoc d22 t2 example",
+			args: args{
+				p1Deck: []int{9, 2, 6, 3, 1},
+				p2Deck: []int{5, 8, 4, 7, 10},
+				level:  1,
+			},
+			want:  2,
+			want1: []int{},
+			want2: []int{7, 5, 6, 2, 4, 1, 10, 8, 9, 3},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1, got2 := game(tt.args.p1Deck, tt.args.p2Deck, tt.args.level)
+			if got != tt.want {
+				t.Errorf("game() got = %v, want %v", got, tt.want)
+			}
+			if !reflect.DeepEqual(got1, tt.want1) {
+				t.Errorf("game() got1 = %v, want %v", got1, tt.want1)
+			}
+			if !reflect.DeepEqual(got2, tt.want2) {
+				t.Errorf("game() got2 = %v, want %v", got2, tt.want2)
+			}
 		})
 	}
 }

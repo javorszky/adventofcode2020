2adb2b544d704dda0cc125c04cabd95c4c3af6cb
Gabor Javorszky <gabor@javorszky.co.uk>
2020-12-23 23:30:25 +0000

    day 23 task 1 done

--- day23/task1.go
+++ day23/task1.go
@@ -1,5 +1,119 @@
 package day23
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 func task1() {
-	_ = getInputs()
+	start := getInputs()
+	for i := 0; i < 100; i++ {
+		start = round(start)
+	}
+
+	fmt.Printf("\nDay 23 task 1: after 100 rounds of Crab Cups the final order is going to be '%s'\n", orderOfCups(start))
+}
+
+// rotateCircleBy does one thing: takes the first element off of slice, sticks it in the end, and returns it.
+func rotateCircleBy(in []int, n int) []int {
+	if n < 1 {
+		return in
+	}
+	realRotation := n % len(in)
+	return append(in[realRotation:], in[0:realRotation]...)
+}
+
+// removeThreeCups will remove three elements from the slice. The elements removed are always the 1,2,3 indexed parts,
+// because in our solution the 0 idx element is the "current" cup.
+func removeThreeCups(in []int) ([]int, []int) {
+	snip := make([]int, 3)
+	short := make([]int, len(in)-3)
+	copy(snip, in[1:4])
+	copy(short, append(in[0:1], in[4:]...))
+	return short, snip
+}
+
+// findIndexOfNextCurrentCup will return the index of the next current cup.
+func findIndexOfNextCurrentCup(in []int) int {
+	// current cup is the 0 idx one
+	current := in[0]
+	existenceHelper := make(map[int]int, 0)
+	highest := 0
+	for idx, v := range in {
+		if v > highest {
+			highest = v
+		}
+		existenceHelper[v] = idx
+	}
+
+	trying := current - 1
+	for {
+		if trying < 0 {
+			trying = highest
+		}
+		idx, ok := existenceHelper[trying]
+		if ok {
+			return idx
+		}
+		trying--
+	}
+}
+
+// insertSnippetIntoAt will insert a slice into another slice at a certain index. This is somewhat counterintuitive,
+// because given a slice of [1 2 3 4], index 3 is the number 4. Inserting [5 6] at index 3 will result in [5 6]
+// becoming the thing at index 3, and whatever was at index 3 (the number 4), comes at the end, so the end result is
+// [1 2 3 5 6 4].
+//
+// To insert something at the front, use idx 0.
+// To insert something at the end, use idx len(short).
+func insertSnippetIntoAt(short, snippet []int, idx int) []int {
+	if idx > len(short) {
+		panic(fmt.Sprintf("can't insert stuff at index %d into a slice with lenght of %d, out of bounds", idx, len(short)))
+	}
+
+	head := make([]int, len(short[0:idx]))
+	tail := make([]int, len(short[idx:]))
+	copy(tail, short[idx:])
+	copy(head, short[0:idx])
+
+	intermediary := append(head, snippet...)
+	intermediary = append(intermediary, tail...)
+
+	return intermediary
+}
+
+// round will play a round of Crab Cups per the rules of Advent Of Code 2020 Day 23 Task 1.
+func round(in []int) []int {
+
+	// n=0 idx is the "current" cup
+	// step 1: remove the snippet
+	short, snippet := removeThreeCups(in)
+	// n+1,2,3 are picked up and removed from the circle
+	nextCupIDX := findIndexOfNextCurrentCup(short)
+
+	newState := insertSnippetIntoAt(short, snippet, nextCupIDX+1)
+
+	// rotate short so that next cup is at idx 0
+	newState = rotateCircleBy(newState, 1)
+
+	// add back the snippet
+
+	return newState
+}
+
+func orderOfCups(in []int) string {
+	idx := 0
+	for i, v := range in {
+		if v == 1 {
+			idx = i
+			break
+		}
+	}
+	ordered := rotateCircleBy(in, idx)[1:]
+	var sb strings.Builder
+	for _, v := range ordered {
+		sb.WriteString(strconv.Itoa(v))
+	}
+	return sb.String()
 }
---
+++ day23/task1_test.go
@@ -0,0 +1,261 @@
+package day23
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_rotateCircleBy(t *testing.T) {
+	type args struct {
+		in []int
+		n  int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "rotates an array by 1",
+			args: args{
+				in: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0},
+				n:  1,
+			},
+			want: []int{2, 3, 4, 5, 6, 7, 8, 9, 0, 1},
+		},
+		{
+			name: "rotates an array by 0",
+			args: args{
+				in: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0},
+				n:  0,
+			},
+			want: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0},
+		},
+		{
+			name: "rotates an array by 4",
+			args: args{
+				in: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0},
+				n:  4,
+			},
+			want: []int{5, 6, 7, 8, 9, 0, 1, 2, 3, 4},
+		},
+		{
+			name: "rotates an array by 12",
+			args: args{
+				in: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0},
+				n:  12,
+			},
+			want: []int{3, 4, 5, 6, 7, 8, 9, 0, 1, 2},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, rotateCircleBy(tt.args.in, tt.args.n))
+		})
+	}
+}
+
+func Test_removeThreeCups(t *testing.T) {
+	type args struct {
+		in []int
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  []int
+		want1 []int
+	}{
+		{
+			name: "removes three cups from the circle where the idx 0 is the current one",
+			args: args{
+				in: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0},
+			},
+			want:  []int{1, 5, 6, 7, 8, 9, 0},
+			want1: []int{2, 3, 4},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := removeThreeCups(tt.args.in)
+			assert.Equal(t, tt.want, got)
+			assert.Equal(t, tt.want1, got1)
+		})
+	}
+}
+
+func Test_findIndexOfNextCurrentCup(t *testing.T) {
+	type args struct {
+		in []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "finds the next current cup when all elements are present",
+			args: args{
+				in: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0},
+			},
+			want: 9,
+		},
+		{
+			name: "finds the next current cup when all elements are present, order is random",
+			args: args{
+				in: []int{1, 9, 5, 7, 3, 0, 2, 4, 8, 6},
+			},
+			want: 5,
+		},
+		{
+			name: "finds the next current cup when some elements are missing, order is random",
+			args: args{
+				in: []int{1, 5, 7, 3, 2, 4, 6},
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, findIndexOfNextCurrentCup(tt.args.in))
+		})
+	}
+}
+
+func Test_insertSnippetIntoAt(t *testing.T) {
+	type args struct {
+		short   []int
+		snippet []int
+		idx     int
+	}
+	tests := []struct {
+		name       string
+		args       args
+		want       []int
+		wantsPanic bool
+	}{
+		{
+			name: "inserts slice into other slice at index 0",
+			args: args{
+				short:   []int{1, 2, 3, 4, 5, 6, 7},
+				snippet: []int{8, 9, 0},
+				idx:     0,
+			},
+			want:       []int{8, 9, 0, 1, 2, 3, 4, 5, 6, 7},
+			wantsPanic: false,
+		},
+		{
+			name: "inserts slice into other slice at index 4",
+			args: args{
+				short:   []int{1, 2, 3, 4, 5, 6, 7},
+				snippet: []int{8, 9, 0},
+				idx:     4,
+			},
+			want:       []int{1, 2, 3, 4, 8, 9, 0, 5, 6, 7},
+			wantsPanic: false,
+		},
+		{
+			name: "inserts slice into other slice at index 6",
+			args: args{
+				short:   []int{1, 2, 3, 4, 5, 6, 7},
+				snippet: []int{8, 9, 0},
+				idx:     6,
+			},
+			want:       []int{1, 2, 3, 4, 5, 6, 8, 9, 0, 7},
+			wantsPanic: false,
+		},
+		{
+			name: "inserts slice into other slice at index 7",
+			args: args{
+				short:   []int{1, 2, 3, 4, 5, 6, 7},
+				snippet: []int{8, 9, 0},
+				idx:     7,
+			},
+			want:       []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0},
+			wantsPanic: false,
+		},
+		{
+			name: "panics when trying to insert at index higher than the length of the original slice",
+			args: args{
+				short:   []int{1, 2, 3, 4, 5, 6, 7},
+				snippet: []int{8, 9, 0},
+				idx:     19,
+			},
+			want:       nil,
+			wantsPanic: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.wantsPanic {
+				assert.Panics(t, func() {
+					insertSnippetIntoAt(tt.args.short, tt.args.snippet, tt.args.idx)
+				})
+				return
+			}
+			assert.Equal(t, tt.want, insertSnippetIntoAt(tt.args.short, tt.args.snippet, tt.args.idx))
+		})
+	}
+}
+
+func Test_round(t *testing.T) {
+	type args struct {
+		in []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "plays a round (round 1 example) correctly",
+			args: args{
+				in: []int{3, 8, 9, 1, 2, 5, 4, 6, 7},
+			},
+			want: []int{2, 8, 9, 1, 5, 4, 6, 7, 3},
+		},
+		{
+			name: "plays a round (round 2 example) correctly",
+			args: args{
+				in: []int{2, 8, 9, 1, 5, 4, 6, 7, 3},
+			},
+			want: []int{5, 4, 6, 7, 8, 9, 1, 3, 2},
+		},
+		{
+			name: "plays a round correctly",
+			args: args{
+				in: []int{5, 4, 6, 7, 8, 9, 1, 3, 2},
+			},
+			want: []int{8, 9, 1, 3, 4, 6, 7, 2, 5},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, round(tt.args.in))
+		})
+	}
+}
+
+func Test_orderOfCups(t *testing.T) {
+	type args struct {
+		in []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "gets correct order of cups from slice",
+			args: args{
+				in: []int{5, 8, 3, 7, 4, 1, 9, 2, 6},
+			},
+			want: "92658374",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, orderOfCups(tt.args.in))
+		})
+	}
+}

ca4cf93fc2dd8d5e95ba75ab9bc7adfca1da2c24
Gabor Javorszky <gabor@javorszky.co.uk>
2020-12-24 22:25:34 +0000

    day 24 task 2 wip

--- day24/task2.go
+++ day24/task2.go
@@ -1,5 +1,72 @@
 package day24
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+const (
+	black = "black"
+	white = "white"
+)
+
+var wheel = []string{se, sw, ne, nw, e, w}
+
 func task2() {
 	_ = getInputs()
+}
+
+func getWorldBlackWhite(in map[string]int) map[string]string {
+	out := make(map[string]string, 0)
+	for k, v := range in {
+		if v%2 == 0 {
+			out[k] = white
+			continue
+		}
+
+		out[k] = black
+	}
+	return out
+}
+
+func getAdjacentAddresses(s string) []string {
+	dirs := make([]string, 0)
+	d := getDirectionsFromCoordinate(s)
+
+	for _, dir := range wheel {
+		// make a copy
+		e := make(directions, 0)
+		for k, v := range d {
+			e[k] = v
+		}
+		e[dir]++
+		dirs = append(dirs, getCoordinate(collapseDirections(e)))
+	}
+
+	return dirs
+}
+
+func getDirectionsFromCoordinate(coordinate string) directions {
+	dirs := strings.Split(coordinate, ".")
+	if len(dirs) != 6 {
+		panic(fmt.Sprintf("receives coordinate does not have 6 elements; '%s'", coordinate))
+	}
+	ints := make([]int, 0)
+	for _, v := range dirs {
+		n, err := strconv.Atoi(v)
+		if err != nil {
+			panic(fmt.Sprintf("could not turn '%s' into an int", v))
+		}
+		ints = append(ints, n)
+	}
+
+	return directions{
+		ne: ints[0],
+		e:  ints[1],
+		se: ints[2],
+		sw: ints[3],
+		w:  ints[4],
+		nw: ints[5],
+	}
 }
---
+++ day24/task2_test.go
@@ -0,0 +1,126 @@
+package day24
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_getWorldBlackWhite(t *testing.T) {
+	type args struct {
+		in map[string]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want map[string]string
+	}{
+		{
+			name: "turns a world into colors",
+			args: args{
+				in: map[string]int{
+					"1": 0,
+					"2": 1,
+					"3": 2,
+					"4": 89,
+					"5": 110,
+				},
+			},
+			want: map[string]string{
+				"1": white,
+				"2": black,
+				"3": white,
+				"4": black,
+				"5": white,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, getWorldBlackWhite(tt.args.in))
+		})
+	}
+}
+
+func Test_getDirectionsFromCoordinate(t *testing.T) {
+	type args struct {
+		d directions
+	}
+	tests := []struct {
+		name string
+		args args
+		want directions
+	}{
+		{
+			name: "converts directions to coordinate, then back to directions",
+			args: args{
+				d: directions{
+					se: 4,
+					sw: 3,
+					ne: 1,
+					nw: 4,
+					e:  2,
+					w:  7,
+				},
+			},
+			want: directions{
+				se: 4,
+				sw: 3,
+				ne: 1,
+				nw: 4,
+				e:  2,
+				w:  7,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, getDirectionsFromCoordinate(getCoordinate(tt.args.d)))
+		})
+	}
+}
+
+func Test_getAdjacentAddresses(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "gets neighbours for origin tile",
+			args: args{
+				s: "0.0.0.0.0.0",
+			},
+			want: []string{
+				"1.0.0.0.0.0",
+				"0.1.0.0.0.0",
+				"0.0.1.0.0.0",
+				"0.0.0.1.0.0",
+				"0.0.0.0.1.0",
+				"0.0.0.0.0.1",
+			},
+		},
+		{
+			name: "gets neighbours for a tile wsw to origin",
+			args: args{
+				s: "0.0.0.3.2.0",
+			},
+			want: []string{
+				"0.0.0.2.2.0", // confirmed
+				"0.0.0.3.1.0", // confirmed
+				"0.0.0.4.1.0", // confirmed
+				"0.0.0.4.2.0", // confirmed
+				"0.0.0.3.3.0", // confirmed
+				"0.0.0.2.3.0", // confirmed
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.ElementsMatch(t, tt.want, getAdjacentAddresses(tt.args.s))
+		})
+	}
+}

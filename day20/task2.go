eb4705e4886d790f0eab56d65d80b3faf951de11
Gabor Javorszky <gabor@javorszky.co.uk>
2020-12-26 01:05:03 +0000

    day 20 task 2 done

--- day20/task2.go
+++ day20/task2.go
@@ -55,126 +55,16 @@ func seaRoughness(stitchedImage []string) int {
 	flattenedImage, lineLength := flattenImage(stitchedImage)
 	seamonsters := newSeaMonsters(seaMonsterPattern, lineLength)
 	for _, seamonster := range seamonsters {
-		replaceMonsters := make([][]int, 0)
-		for idx, char := range flattenedImage {
-			lastOffset := seamonster.Offsets[len(seamonster.Offsets)-1]
-			lenFlattenedImage := len(flattenedImage)
-			// skip characters that are either too close to the left, or too close to the right edge, or are not hash.
-			// We're looking to find all the hashes that can be starter points for sea monsters.
-			if idx%lineLength > lineLength-seamonster.W+seamonster.First || // it's not too close to the right side.
-				idx%lineLength < seamonster.First || // not too close to the left side
-				idx+lastOffset > lenFlattenedImage || // make sure the last part of the sea monster is still in pic.
-				string(char) != "#" { // not a hash character
-				continue
-			}
-
-			//checkCoords = append(checkCoords, idx)
-
-			//for each hash that is in the zone of suitable candidates, let's assume we found the monster.
-			foundMonster := true
-
-			// and make sure we save the coordinates for this monster, so we can change them later.
-			monsterCoords := make([]int, 0, 15)
-
-			// let's step through the offsets for the seamonster we're trying to find, and check whether all the other
-			// characters are also #. If not, break, and move on to the next character.
-			for _, offset := range seamonster.Offsets {
-				//fmt.Printf("checking for character %d at offset %d with total offset %d\n", sidx, offset, offset+idx)
-				// the next char at specified offset from first is not a hash, break off.
-				if flattenedImage[idx+offset-1:idx+offset] != "#" {
-					foundMonster = false
-					break
-				}
-				monsterCoords = append(monsterCoords, idx+offset)
-			}
-			if foundMonster {
-				replaceMonsters = append(replaceMonsters, monsterCoords)
-			}
-		}
-
-		fmt.Printf("number of monsters found: %d\n", len(replaceMonsters))
-
-		for _, monsterCoords := range replaceMonsters {
-			flattenedImage = replaceMonster(flattenedImage, monsterCoords)
+		monsterCoords := findMonsterCoords(flattenedImage, lineLength, seamonster)
+		for _, coords := range monsterCoords {
+			flattenedImage = replaceMonster(flattenedImage, coords)
 		}
 	}
 
 	return strings.Count(flattenedImage, "#")
 }
 
-func seaRoughnessRotatedMap(stitchedImage []string) int {
-	images := [][]string{
-		stitchedImage,
-		rotateContent(stitchedImage),
-		rotateContent(rotateContent(stitchedImage)),
-		rotateContent(rotateContent(rotateContent(stitchedImage))),
-		hFlipContent(stitchedImage),
-		rotateContent(hFlipContent(stitchedImage)),
-		rotateContent(rotateContent(hFlipContent(stitchedImage))),
-		rotateContent(rotateContent(rotateContent(hFlipContent(stitchedImage)))),
-	}
-
-	monstar := strings.Split(seaMonsterPattern, "\n")
-	first, offsets := seaMonsterOffsets(monstar, len(stitchedImage[0]))
-	seaMonster := seaMonster{
-		W:       len(monstar[0]),
-		H:       len(monstar),
-		First:   first,
-		Offsets: offsets,
-	}
-
-	for _, image := range images {
-		flattenedImage, lineLength := flattenImage(image)
-
-		replaceMonsters := make([][]int, 0)
-		for idx, char := range flattenedImage {
-			lastOffset := seaMonster.Offsets[len(seaMonster.Offsets)-1]
-			lenFlattenedImage := len(flattenedImage)
-			// skip characters that are either too close to the left, or too close to the right edge, or are not hash.
-			// We're looking to find all the hashes that can be starter points for sea monsters.
-			if idx%lineLength > lineLength-seaMonster.W+seaMonster.First || // it's not too close to the right side.
-				idx%lineLength < seaMonster.First || // not too close to the left side
-				idx+lastOffset > lenFlattenedImage || // make sure the last part of the sea monster is still in pic.
-				string(char) != "#" { // not a hash character
-				continue
-			}
-
-			//checkCoords = append(checkCoords, idx)
-
-			//for each hash that is in the zone of suitable candidates, let's assume we found the monster.
-			foundMonster := true
-
-			// and make sure we save the coordinates for this monster, so we can change them later.
-			monsterCoords := make([]int, 0, 15)
-
-			// let's step through the offsets for the seamonster we're trying to find, and check whether all the other
-			// characters are also #. If not, break, and move on to the next character.
-			for _, offset := range seaMonster.Offsets {
-				//fmt.Printf("checking for character %d at offset %d with total offset %d\n", sidx, offset, offset+idx)
-				// the next char at specified offset from first is not a hash, break off.
-				if flattenedImage[idx+offset-1:idx+offset] != "#" {
-					foundMonster = false
-					break
-				}
-				monsterCoords = append(monsterCoords, idx+offset)
-			}
-			if foundMonster {
-				replaceMonsters = append(replaceMonsters, monsterCoords)
-			}
-		}
-
-		fmt.Printf("number of monsters found: %d\n", len(replaceMonsters))
-
-		for _, monsterCoords := range replaceMonsters {
-			flattenedImage = replaceMonster(flattenedImage, monsterCoords)
-		}
-	}
-
-	return 0
-}
-
 func findMonsterCoords(flattenedImage string, lineLength int, seaMonster seaMonster) [][]int {
-	fmt.Printf("%#v\n", seaMonster.Offsets)
 	replaceMonsters := make([][]int, 0)
 	for idx, char := range flattenedImage {
 		lastOffset := seaMonster.Offsets[len(seaMonster.Offsets)-1]
@@ -187,7 +77,6 @@ func findMonsterCoords(flattenedImage string, lineLength int, seaMonster seaMons
 			string(char) != "#" { // not a hash character
 			continue
 		}
-		fmt.Printf("found hash char at idx %d: '%s'\n", idx, flattenedImage[idx:idx+1])
 
 		//checkCoords = append(checkCoords, idx)
 
@@ -203,10 +92,8 @@ func findMonsterCoords(flattenedImage string, lineLength int, seaMonster seaMons
 			//fmt.Printf("checking for character %d at offset %d with total offset %d\n", sidx, offset, offset+idx)
 			// the next char at specified offset from first is not a hash, break off.
 			shiftedMonsterOffset := idx + offset
-			fmt.Printf("checking char at offset %d, which is '%s'\n", shiftedMonsterOffset, flattenedImage[shiftedMonsterOffset:shiftedMonsterOffset+1])
 			if flattenedImage[shiftedMonsterOffset:shiftedMonsterOffset+1] != "#" {
 				foundMonster = false
-				fmt.Printf("whoops, did not find a monster part, aborting...\n")
 				break
 			}
 			monsterCoords = append(monsterCoords, idx+offset)
